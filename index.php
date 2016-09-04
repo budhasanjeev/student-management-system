e1c4b0102e5251895cd36ca62300728e080ecd07
Sanjeev Budha <budhasanjeev@gmail.com>
2016-09-04 14:09:44 +0545

    second commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sanjeev
+ * Date: 9/4/2016
+ * Time: 2:04 PM
+ */
+
+header('Location:/views/home.php');
---
+++ views/home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sanjeev
+ * Date: 9/4/2016
+ * Time: 2:04 PM
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+    
+    <?php
+    
+        
+    ?>
+    </body>
+
+</html>
