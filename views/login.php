52abf2a6116d29deea2e88271f3369a0bad368f1
Pratik Budhathoki <pratik.budhathoki@deerwalk.edu.p>
2016-09-04 21:02:36 +0545

    login page design

--- views/login.php
+++ views/login.php
@@ -6,6 +6,36 @@
  * Time: 11:55 AM
  */
 
+include '../config/databaseConnection.php';
+if (isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $stored_password = "null";
+    $role = "null";
+
+    $select_query = "SELECT `password`, `role` FROM `user` WHERE `email_address` = '$email'";
+    $result = $connection->query($select_query);
+
+
+    while($row = $result->fetch_assoc()){
+        $stored_password = $row['password'];
+        $role = $row['role'];
+    }
+
+    if($password == $stored_password){
+        if($role == "admin"){
+            header("Location: admin/index.php");
+        }else{
+            echo "student";
+        }
+    }else{
+        echo "user not found";
+    }
+
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -19,12 +49,12 @@
 <body style="display: flex; justify-content: center; align-items: center;">
 
 <div class="highlight login-div">
-    <form class="form-horizontal" action="">
+    <form class="form-horizontal"  method="post" action="">
         <legend><h2>System Login</h2></legend>
         <div class="form-group">
-            <label class="glyphicon glyphicon-user col-md-2 login-glyphicon"></label>
+            <label class="glyphicon glyphicon-envelope col-md-2 login-glyphicon"></label>
             <div class="col-md-10">
-                <input type="text" class="form-control" name="username" required="" placeholder="username"/>
+                <input type="email" class="form-control" name="email" required="" placeholder="username"/>
             </div>
         </div>
 
@@ -35,7 +65,7 @@
             </div>
         </div>
 
-        <input class="btn btn-primary btn-block" type="submit" value="login"/>
+        <input class="btn btn-primary btn-block" name="login" type="submit" value="login"/>
     </form>
 </div>
 
