8c8ee22315bc01c4da2af78745d5f823eb58015b
Pratik Budhathoki <pratik.budhathoki@deerwalk.edu.p>
2016-09-05 22:01:26 +0545

    admin page

--- views/admin/student.php
+++ views/admin/student.php
@@ -5,3 +5,29 @@
  * Date: 9/5/2016
  * Time: 6:38 PM
  */
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Student Management</title>
+</head>
+<body>
+<?php
+include '../layout/header.php';
+?>
+
+<div class="container">
+    <div class="col-md-6">
+        <a href="studentForm.php" class="btn btn-block btn-primary">Form to add student</a>
+    </div>
+    <div class="col-md-6">
+        <a href="" class="btn btn-block btn-primary">Excell to add student</a>
+    </div>
+</div>
+
+
+
+</body>
+</html>
