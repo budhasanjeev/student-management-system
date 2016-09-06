5ad73bba427cf993a248fdaf45276ccf760d062b
anijor <anijorshrestha@gmail.com>
2016-09-06 17:06:19 +0545

    Attendance Upload

--- views/admin/attendance.php
+++ views/admin/attendance.php
@@ -5,3 +5,18 @@
  * Date: 9/5/2016
  * Time: 6:39 PM
  */
+?>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Attendance</title>
+</head>
+<body>
+    <form action="../../controller/attendanceController.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" value="upload">
+
+    </form>
+</body>
+</html>
+
