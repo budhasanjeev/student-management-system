bc2bd23fe13d83049eadf85002e9b222134dd27c
Pratik Budhathoki <pratik.budhathoki@deerwalk.edu.p>
2016-09-16 17:11:39 +0545

    teacher attendance page

--- views/teacher.php
+++ views/teacher.php
@@ -20,7 +20,112 @@
 <body>
 <?php include 'layout/header.php'; ?>
 <div class="container">
+    <div class="col-md-1">
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 1</a>
+        <a class="btn btn-block btn-block" href="">Class 10</a>
+    </div>
+    <div class="col-md-11">
+        <legend>Attendance</legend>
 
+        <form action="">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Ram</th>
+                <th>Hari</th>
+                <th>Ram</th>
+                <th>Ram</th>
+                <th>Ram</th>
+                <th>Ram</th>
+                <th>Ram</th>
+                <th>Hari</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+            <tr>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+                <td><select name="attendance">
+                        <option value="volvo">Absent</option>
+                        <option value="saab">Present</option>
+                        <option value="saab">Leave</option>
+                    </select>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+            <input class="btn btn-primary pull-right" value="Done" type="submit"/>
+        </form>
+    </div>
 </div>
 </body>
 </html>
