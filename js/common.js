b79af302a8e1fcafd484f31397298eb3bc4a0eb4
anijor <anijorshrestha@gmail.com>
2016-09-16 18:48:01 +0545

    student form validation

---
+++ js/common.js
@@ -0,0 +1,38 @@
+/**
+ * Created by anijor on 9/16/2016.
+ */
+
+
+
+
+function checkAlphabet(id) {
+
+    var input = $('#' + id).val();
+
+    var alphaExp = /^[a-zA-Z]+$/;
+
+    if (input.match(alphaExp)) {
+        return true;
+
+    }
+    else {
+        alert("Invalid");
+        return false;
+    }
+}
+
+function checkNumeric(id)
+{
+    var input=$('#' + id).val();
+    var numericExpression = /^[0-9]+$/;
+    if (input.match(numericExpression)) {
+        return true;
+
+    }
+    else {
+        alert("Invalid");
+        return false;
+    }
+}
+
+
--- views/studentForm.php
+++ views/studentForm.php
@@ -13,57 +13,59 @@
     <meta charset="UTF-8">
     <title>Student Management</title>
 
+    <script src="../js/common.js"></script>
+
+
 </head>
 <body>
 <?php include 'layout/header.php' ?>
 <div class="container">
     <legend>Add Student</legend>
-    <form action="../controller/studentController.php" method="post" enctype="multipart/form-data">
+    <form action="../controller/studentController.php"  name="studentform" method="post"   enctype="multipart/form-data">
         <fieldset>
             <h4>Students Detail</h4>
             <input type="hidden" name="mode" id="modes" value="add">
-            <input type="hidden" name="student_id" id="student_id">
             <div class="col-md-4">
-                <input type="text" name="student_id" class="form-control form-group" placeholder="ID" />
+                <input type="text" name="student_id" id="student_id" class="form-control form-group" placeholder="ID" onkeyup="checkNumeric('student_id')" />
             </div>
             <div class="col-md-4">
-                <input type="text" name="firstName" class="form-control form-group" placeholder="First Name" id="firstName" />
+                <input type="text" name="firstName" class="form-control form-group" placeholder="First Name" id="firstName" onkeyup="checkAlphabet('firstName')"/>
             </div>
             <div class="col-md-4">
-                <input type="text" name="lastName" placeholder="last Name" class="form-control form-group" id="lastName"/>
+                <input type="text" name="lastName" placeholder="last Name" class="form-control form-group" id="lastName" onkeyup="checkAlphabet('lastName')"/>
             </div>
             <div class="col-md-4">
                 <input type="date" name="dob" placeholder="DOB" class="form-control form-group" id="dob" />
             </div>
             <div class="col-md-4">
-                <input type="text" name="address" placeholder="address" class="form-control form-group" id="address" />
+                <input type="text" name="address" placeholder="address" class="form-control form-group" id="address" onkeyup="checkAlphabet('address')"/>
             </div>
             <div class="col-md-4">
-                <input type="tel" name="contact" placeholder="contact" class="form-control form-group" id="contact" />
+                <input type="tel" name="contact" placeholder="contact" class="form-control form-group" id="contact" onkeyup="checkNumeric('contact')"/>
             </div>
             <div class="col-md-4">
-                <input type="number" name="rollNumber" placeholder="roll no." class="form-control form-group" id="roll"/>
+                <input type="number" name="rollNumber" placeholder="roll no." class="form-control form-group" id="roll"  onkeyup="checkNumeric('roll')"  />
             </div>
             <div class="col-md-4">
-                <input type="text" name="grade" placeholder="grade" class="form-control form-group" id="grade"/>
+                <input type="text" name="grade" placeholder="grade" class="form-control form-group" id="grade"  onkeyup="checkNumeric('grade')"/>
             </div>
             <div class="col-md-4">
-                <input type="text" name="section" placeholder="section" class="form-control form-group" id="section"/>
+                <input type="text" name="section" placeholder="section" class="form-control form-group" id="section" onkeyup="checkAlphabet('section')"/>
             </div>
         </fieldset>
         <h4>Parents Detail</h4>
         <div class="col-md-6">
-            <input type="text" name="fatherName" class="form-control form-group" placeholder="Father Name" id="fatherName" />
+            <input type="text" name="fatherName" class="form-control form-group" placeholder="Father Name" id="fatherName"  onkeyup="checkAlphabet('fathername')" />
         </div>
         <div class="col-md-6">
-            <input type="text" name="motherName" class="form-control form-group" placeholder="Mother Name" id="motherName"/>
+            <input type="text" name="motherName" class="form-control form-group" placeholder="Mother Name" id="motherName" onkeyup="checkAlphabet('mothername')" />
         </div>
         <h4>Photo</h4>
-        <div class="col-md-4 row">
+        <div class="col-md-4">
             <input type="file" name="photo" class="form-group" id="studentPhoto"/>
         </div>
-        <div class="row" style="text-align: right;">
-            <input class="btn btn-primary" name="submit" type="submit" value="Add"/>
+        <div class="col-md-12">
+            <input class="btn btn-primary btn-block" name="submit"  type="submit"  value="Add"/>
         </div>
     </form>
 </div>
